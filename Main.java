d6ea221c3eae60bf4d6681db766cbaaea5ba353a
jeandersonlima <161890332+jeandersonlima@users.noreply.github.com>
2025-11-19 11:12:28 -0300

    Add files via upload
    Signed-off-by: jeandersonlima <161890332+jeandersonlima@users.noreply.github.com>

--- Main.java
+++ Main.java
@@ -5,18 +5,20 @@ import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) {
-		
+
 		// TESTE DE AVALIAÇÃO
-		//Cliente pessoa = new Cliente("Carlos", 29, "23", "c@c", "123", "M");
-		//pessoa.consultarMensalidade();
-		/*Psicologo psicologo = new Psicologo("Joao", 47, "33", "e@e", "321", "M");
-		pessoa.avaliarPsicologo(psicologo, 5);
-		pessoa.avaliarPsicologo(psicologo, 7);
-		System.out.println("Avaliação média do psicológo:"+ psicologo.getAvaliacaoMedia()); */
+		// Cliente pessoa = new Cliente("Carlos", 29, "23", "c@c", "123", "M");
+		// pessoa.consultarMensalidade();
+		/*
+		 * Psicologo psicologo = new Psicologo("Joao", 47, "33", "e@e", "321", "M");
+		 * pessoa.avaliarPsicologo(psicologo, 5); pessoa.avaliarPsicologo(psicologo, 7);
+		 * System.out.println("Avaliação média do psicológo:"+
+		 * psicologo.getAvaliacaoMedia());
+		 */
 		// FIM DO TESTE
 		Empresa empresa = new Empresa("PsicoInsight", "38.514.927/0001-62", "corporativo@psicinsight.com",
 				"PsiC0rp!2025");
-		//TESTE DE AVALIAÇÃO
+		// TESTE DE AVALIAÇÃO
 		Cliente pessoa = new Cliente("Carlos", 29, "23", "c@c", "123", "M");
 		Psicologo psicologos = new Psicologo("Joao", 47, "33", "e@e", "321", "M");
 		pessoa.avaliarPsicologo(psicologos, 5);
@@ -26,7 +28,7 @@ public class Main {
 		pessoa.avaliarPsicologo(psicologos, 8);
 		pessoa.avaliarPsicologo(psicologos, 5);
 		pessoa.avaliarPsicologo(psicologos, 10);
-		
+
 		Scanner scan = new Scanner(System.in);
 		System.out.println(" Seja bem-vindo ao Psicolink!! \n " + "qual o seu formato de entrada no sistema: \n"
 				+ "[1] Cliente \n" + "[2] Psicologo \n" + "[3] Empresa");
@@ -57,33 +59,44 @@ public class Main {
 
 			Cliente cliente1 = new Cliente(nome, idade_c, senha_c, email_c, cpf_c, sexo_c);
 
-			System.out.println("OK! tudo pronto " + cliente1.getNome());
-			System.out.println("Deseja utilizar alguma função?");
-			System.out.println("[1] Meu diário pessoal. \n"
-					+ "[2] Avaliar psicólogo \n"
-					+ "[3] Consultar mensalidade do consultório");
-			int esc_c = scan.nextInt();
-			scan.nextLine();
-			if(esc_c == 1) {
-				Cliente.Anotacao_c(); 
-				Cliente.mostrarAnotacoes_c();
-			}
-				
-		
-			if (esc_c == 2) {
-				System.out.println("== SISTEMA DE AVALIAÇÃO DO PSICÓLOGO");
-				int test_p = scan.nextInt();
+			while (true) {
+				System.out.println("OK! tudo pronto " + cliente1.getNome());
+				System.out.println("Deseja utilizar alguma função?");
+				System.out.println("[1] Meu diário pessoal. \n" + "[2] Avaliar psicólogo \n"
+						+ "[3] Consultar mensalidade do consultório");
+				int esc_c = scan.nextInt();
 				scan.nextLine();
-				
-			}
-			
-			if(esc_c == 3) {
-			System.out.println("== SISTEMA DE CONSULTA DE MENSALIDADE ==");
-			Cliente.consultarMensalidade();
+				if (esc_c == 1) {
+					Cliente.Anotacao_c();
+					Cliente.mostrarAnotacoes_c();
+				}
+
+				if (esc_c == 2) {
+					System.out.println("== SISTEMA DE AVALIAÇÃO DO PSICÓLOGO");
+					System.out.println("De 0 a 10 qual o nivel de satisfação com o seu psicólogo?");
+					int avaliacao = scan.nextInt();
+					scan.nextLine();
+					cliente1.avaliarPsicologo();
+					
+
+				}
+
+				if (esc_c == 3) {
+					System.out.println("== SISTEMA DE CONSULTA DE MENSALIDADE ==");
+					Cliente.consultarMensalidade();
+				}
+
+				System.out.println(" ");
+				System.out.printf("Deseja sair do sistema de Cliente? %s (S/N): ", cliente1.getNome());
+				String escolha_sair = scan.nextLine();
+				if (escolha_sair.equalsIgnoreCase("S")) {
+					System.out.println("=== OBRIGADO POR UTILIZAR PSICOLINK ==");
+					break;
+				}
 			}
 		}
 		// Psicologo
-			if (entrada == 2) {
+		if (entrada == 2) {
 
 			// Caso uma empresa se cadastre vai funcionar do msm jeito, mas com os dados da
 			// empresa
@@ -102,8 +115,6 @@ public class Main {
 			String sexo_p = scan.nextLine();
 
 			Psicologo psicologo1 = new Psicologo(nome_p, idade_p, "1111", "email@alter", cpf_p, sexo_p);
-			
-			
 
 			// Criação de uma empresa genérica caso uma não tenha sido cadastrada ainda.
 			// Logo em seguida com a msm são criados email e senha para o psicologo com as
@@ -123,42 +134,46 @@ public class Main {
 			psicologo1.setEmail(email_gerado);
 
 			System.out.println("Seu email e senha da empresa: " + email_gerado + "  " + psicologo1.getCpf());
-			
-			System.out.println("OK! tudo pronto " + psicologo1.getNome());
-			System.out.println("Deseja utilizar alguma função?");
-			System.out.println("[1] Anotações pacientes. \n"
-					+ "[2] Ver suas avaliações. \n"
-					+ "[3] Consultar Salário");
-			int esc_p = scan.nextInt();
-			scan.nextLine();
-			if(esc_p == 1) {
-				System.out.println("== SISTEMA DE ANOTAÇÃO ==");
-				Psicologo.Anotacao_p(); 
-				Psicologo.mostrarAnotacoes();
-			}
-			if(esc_p == 2) {
-				System.out.println("== SISTEMA DE MÉDIA AVALIATIVA ==");
-				System.out.println("Avaliação média do psicológo:"+ psicologos.getAvaliacaoMedia()); 
-			}
+			while (true) {
+				System.out.println("OK! tudo pronto " + psicologo1.getNome());
+				System.out.println("Deseja utilizar alguma função?");
+				System.out.println(
+						"[1] Anotações pacientes. \n" + "[2] Ver suas avaliações. \n" + "[3] Consultar Salário");
+				int esc_p = scan.nextInt();
+				scan.nextLine();
 
-			
-			if(esc_p ==3) {
-				System.out.println("== SISTEMA DE SALÁRIO ==");
-				psicologo1.numeroSessoes();
-				Empresa.calcularSalario(psicologo1);
-			}
+				if (esc_p == 1) {
+					System.out.println("== SISTEMA DE ANOTAÇÃO ==");
+					Psicologo.Anotacao_p();
+					Psicologo.mostrarAnotacoes();
 
-			
+				}
+
+				if (esc_p == 2) {
+					System.out.println("== SISTEMA DE MÉDIA AVALIATIVA ==");
+					System.out.println("Avaliação média do psicológo:" + psicologos.getAvaliacaoMedia());
+				}
+
+				if (esc_p == 3) {
+					System.out.println("== SISTEMA DE SALÁRIO ==");
+					psicologo1.numeroSessoes();
+					Empresa.calcularSalario(psicologo1);
+				}
+
+				System.out.println(" ");
+				System.out.printf("Deseja sair do sistema de psicologo %s? (S/N): ", psicologo1.getNome());
+				String escolha_sair = scan.nextLine();
+				if (escolha_sair.equalsIgnoreCase("S")) {
+					System.out.println("=== OBRIGADO POR UTILIZAR PSICOLINK ==");
+					break;
+				}
 			}
-		
+		}
+
 		// Empresa
 		if (entrada == 3) {
-			Empresa.CriacaoEmpresa();
 			System.out.println("== SISTEMA DE CADASTRO DE EMPRESA ==");
-			System.out.println("");
-							}
-						}
-					}	
-			
-					
-	
+			Empresa.CriacaoEmpresa();
+		}
+	}
+}
